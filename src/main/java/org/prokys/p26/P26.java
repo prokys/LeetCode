ab75b498f3d79460243dae92887c895ef75d7f26
Prokop Ferdan <prokes1@gmail.com>
2024-11-25 13:31:04 +0100

    added solution to problem 26

---
+++ src/main/java/org/prokys/p26/P26.java
@@ -0,0 +1,34 @@
+package org.prokys.p26;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P26 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int num : nums) {
+            if (!numbers.contains(num)) {
+                numbers.add(num);
+            }
+        }
+
+        int numberOfElements = numbers.size();
+
+        for (int i = 0; i < numberOfElements; i++) {
+            nums[i] = numbers.get(i);
+        }
+
+        return numberOfElements;
+    }
+
+}
