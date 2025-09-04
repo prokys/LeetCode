6f1d2bd5dcd3656665398d60701ce96021ddef90
Prokop Ferdan <prokes1@gmail.com>
2025-09-04 13:23:45 +0200

    added solution to p104 that beats 100% in time

---
+++ src/main/java/org/prokys/p104/P104.java
@@ -0,0 +1,32 @@
+package org.prokys.p104;
+
+public class P104 {
+    public static int maxDepthInt = 0;
+    public static TreeNode root = new TreeNode(1, null, new TreeNode(2));
+
+    public static void main(String[] args) {
+        System.out.println(maxDepth(root));
+    }
+    public static int maxDepth(TreeNode root) {
+        maxDepthInt = 0;
+        getDepth(root,0);
+        return maxDepthInt;
+    }
+
+    public static void getDepth(TreeNode root, int depth){
+        depth++;
+        if (root == null) {
+            depth--;
+            return;
+        }
+
+        if (depth > maxDepthInt){
+            maxDepthInt = depth;
+        }
+        getDepth(root.left, depth);
+        getDepth(root.right, depth);
+    }
+
+
+
+}
