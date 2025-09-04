68e491b50672a96a1a5af0e09906027f5e9dc227
Prokop Ferdan <prokes1@gmail.com>
2025-09-04 11:30:59 +0200

    added solution to p100 that beats 100% in time

---
+++ src/main/java/org/prokys/p100/P100.java
@@ -0,0 +1,50 @@
+package org.prokys.p100;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class P100 {
+
+    public static TreeNode root = new TreeNode(2, new TreeNode(1), null);
+    public static TreeNode root2 = new TreeNode(1, null, new TreeNode(2));
+    public static List<Integer> values;
+    public static List<Integer> values2;
+
+    public static void main(String[] args) {
+
+        System.out.println(isSameTree(root, root2));
+        System.out.println(isSameTree(root, root));
+        System.out.println(isSameTree(root2, root2));
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        values = new ArrayList<>();
+        values2 = new ArrayList<>();
+        getList(p);
+        getList2(q);
+
+        return values.equals(values2);
+    }
+
+    private static void getList(TreeNode root) {
+        if (root == null) {
+            values.add(null);
+            return;
+        }
+
+        values.add(root.val);
+        getList(root.left);
+        getList(root.right);
+    }
+    private static void getList2(TreeNode root) {
+        if (root == null) {
+            values2.add(null);
+            return;
+        }
+
+        values2.add(root.val);
+        getList2(root.left);
+        getList2(root.right);
+    }
+}
