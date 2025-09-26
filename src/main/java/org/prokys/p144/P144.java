23ef91305d5311e85d93cc33f7c498aa400c1255
Prokop Ferdan <prokes1@gmail.com>
2025-09-26 13:27:02 +0200

    added solution to p144 that beats 100% in time

---
+++ src/main/java/org/prokys/p144/P144.java
@@ -0,0 +1,32 @@
+package org.prokys.p144;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P144 {
+
+    public static List<Integer> values = new ArrayList<>();
+    public static TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+    public static TreeNode root1 = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5, new TreeNode(6), new TreeNode(7))), new TreeNode(3, null, new TreeNode(8, new TreeNode(9), null)));
+
+    public static void main(String[] args) {
+        System.out.println(preorderTraversal(root));
+        System.out.println(preorderTraversal(root1));
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null){
+            return new ArrayList<>();
+        }
+        values = new ArrayList<>();
+        getList(root);
+        return values;
+    }
+
+    private static void getList(TreeNode root) {
+        if (root == null) return;
+        values.add(root.val);
+        getList(root.left);
+        getList(root.right);
+    }
+}
