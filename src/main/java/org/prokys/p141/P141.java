da8abcd839888684226a1f1895ee7bb3339bc603
Prokop Ferdan <prokes1@gmail.com>
2025-09-26 13:09:21 +0200

    added solution to p141

---
+++ src/main/java/org/prokys/p141/P141.java
@@ -0,0 +1,28 @@
+package org.prokys.p141;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P141 {
+
+    public static List<ListNode> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static boolean hasCycle(ListNode head) {
+
+        if (head.next == null){
+            return false;
+        }else if (list.contains(head)){
+            return true;
+        }else {
+            list.add(head);
+            return hasCycle(head.next) == true;
+        }
+
+    }
+}
