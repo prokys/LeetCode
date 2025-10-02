894092f53d70e2424d1d9acb8e72a1b52ed75b5c
Prokop Ferdan <prokes1@gmail.com>
2025-10-02 13:48:13 +0200

    added solution to p160

---
+++ src/main/java/org/prokys/p160/P160.java
@@ -0,0 +1,46 @@
+package org.prokys.p160;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P160 {
+
+    public static List<ListNode> list1;
+    public static List<ListNode> list2;
+
+    public static ListNode commonNode = new ListNode(8, new ListNode(4, new ListNode(5)));
+    public static ListNode node1 = new ListNode(4, new ListNode(1, commonNode));
+    public static ListNode node2 = new ListNode(5, new ListNode(6, new ListNode(1, commonNode)));
+
+    public static void main(String[] args) {
+
+        System.out.println(getIntersectionNode(node1, node2));
+    }
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        list1 = new ArrayList<>();
+        list2 = new ArrayList<>();
+        getNodes(headA, list1);
+        getNodes(headB, list2);
+
+        for (int i = 0; i < list1.size() ; i++) {
+            if (list2.contains(list1.get(i))){
+
+                return list1.get(i);
+            }
+        }
+
+        return null;
+    }
+
+    public static ListNode getNodes(ListNode node, List<ListNode> inputList){
+        if (node.next != null){
+            inputList.add(node);
+            getNodes(node.next, inputList);
+            return node;
+        }else {
+            inputList.add(node);
+            return node;
+        }
+    }
+}
