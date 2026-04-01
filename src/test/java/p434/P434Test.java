9ad1d8d1ceefe353848b7f9c3ee1124ddbb5602a
Prokop Ferdan <prokes1@gmail.com>
2026-04-01 14:34:35 +0200

    added solution to p434

---
+++ src/test/java/p434/P434Test.java
@@ -0,0 +1,27 @@
+package p434;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.prokys.p434.P434.countSegments;
+
+public class P434Test {
+
+    static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of("Hello, my name is John", 5),
+                Arguments.of("Hello", 1),
+                Arguments.of("Hello   a    Hello", 3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void testCountSegments(String input, int result){
+        Assertions.assertEquals(result, countSegments(input));
+    }
+}
